4911af79703c5c1419e511e7052dc3e0b4f13eba
zigsong <wldms5764@gmail.com>
2021-12-12 21:49:16 +0900

    chore: mockCards 데이터 삽입

---
+++ src/components/CardPane/mock.ts
@@ -0,0 +1,49 @@
+import { CardType } from 'types';
+
+const mockCards: CardType[] = [
+  {
+    id: 1,
+    label: 'Card Label',
+    title: 'Card Title',
+    price: 'Cross Out',
+    isHighlighted: true,
+    stars: 3,
+    artist: 'John Doe',
+    review:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    link: '#',
+  },
+  {
+    id: 2,
+    label: 'Card Label',
+    title: 'Card Title',
+    price: 'Cross Out',
+    isHighlighted: true,
+    stars: 3,
+    artist: 'John Doe',
+    link: '#',
+  },
+  {
+    id: 3,
+    label: 'Card Label',
+    title: 'Card Title',
+    price: 'Cross Out',
+    isHighlighted: true,
+    artist: 'John Doe',
+    link: '#',
+  },
+  {
+    id: 4,
+    label: 'Card Label',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    price: 'Cross Out',
+    isHighlighted: true,
+    artist: 'John Doe',
+    review:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    link: '#',
+  },
+];
+
+export default mockCards;
